84ee328461fcc4ba6cef35de1c4a219a14680952
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-08-31 12:51:36 -0600

    initial steps and some psuedo coding and html layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Psuedo Coding steps----
+
+// Variables---
+
+// Functions----
+
+//Steps I'd like to achieve
+// generate random word probably from array of strings made up of words pirate-y
+// replace letters with _ _ _ _ and have them fill in when users guess === the letter/s in the word. 
+// any wrong guesses are displayed on screen and a strike against them is displayed until theyre out of guesses 
+// Keep it Pirate.
