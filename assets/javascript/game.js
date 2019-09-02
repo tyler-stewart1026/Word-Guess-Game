f4ebd36f4d6f7f65266ca55d6475be03774a6c6d
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-09-01 18:03:28 -0600

    some intial variables and made a button to start game that doesnt work. made random word selector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,28 @@
 // Psuedo Coding steps----
 
 // Variables---
+var wins = 0;
+var losses = 0;
+var lettersGuessed = [];
+var guessesSoFar = [];
+var words = ["Corsair", "Keelhaul", "Parley", "Landlubber", "Cutless", "Ahoy", "Aye", "Parrot", "Brig", "Rum", "Skaliwag", "Scurvy"];
+var randomWord = currentWord();
+console.log(randomWord)
 
-// Functions----
+// start button variables
 
+
+// Button Function----
+document.getElementById("button").onclick = randomWord;
+
+// main function
+
+function currentWord() {
+
+    var text = words[Math.floor(Math.random() * words.length)]
+
+    return text;
+};
 //Steps I'd like to achieve
 // generate random word probably from array of strings made up of words pirate-y
 // replace letters with _ _ _ _ and have them fill in when users guess === the letter/s in the word. 
